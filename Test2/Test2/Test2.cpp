f79b54358e041fbbc88cdde1e96d5b1ed74a8de1
AlexLebed11 <121554480+AlexLebed11@users.noreply.github.com>
2023-11-02 15:39:42 +0200

    Add files via upload

---
+++ Test2/Test2/Test2.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	vector<int> numbers; 
+	string str;            
+	int N,  num, q;
+
+	cin >> N;
+	cin.ignore();
+
+	getline(cin, str); 
+	istringstream ss(str);             
+	while (ss >> num)
+	{ 
+		numbers.push_back(num); 
+	}
+
+	sort(numbers.begin(), numbers.end());	
+
+	if (N % 2 == 0)
+	{
+		q = numbers.at(N / 2 - 1);
+	}
+	else 
+	{
+		q = numbers.at(N / 2) ;
+	}
+
+	cout << q;	
+	return 0;
+}
